bb5c702a1c655915a56992232d7a0b0e3866b99e
SamuelManukonda <samuel_manukonda@epam.com>
2026-03-05 13:04:10 +0530

    update ProductController and products.json with new product data and CORS configuration

--- src/main/java/com/hometask/inventory/controller/ProductController.java
+++ src/main/java/com/hometask/inventory/controller/ProductController.java
@@ -9,15 +9,13 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
 
     private final ProductService productService;
